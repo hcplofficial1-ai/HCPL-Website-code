db878c07bf2b8bc1c65981c85b7c723391e4b4fe
HIMAT Consulting <admin@himatconsulting.com>
2026-09-21 16:47:16 +0500

    Add dedicated laptop and digital workflow image to Office Automation & ERP core competency

--- server/server.js
+++ server/server.js
@@ -293,7 +293,7 @@ async function autoSeedIfEmpty() {
       await Competency.updateOne({ id: 'organizational-assessment' }, { $set: { image: './images/capacity-building-bg.jpg' } })
       await Competency.updateOne({ id: 'capacity-building' }, { $set: { image: './images/organizational-assessment-bg.jpg' } })
       await Competency.updateOne({ id: 'third-party-monitoring' }, { $set: { image: './images/office-automation-erp-bg.jpg' } })
-      await Competency.updateOne({ id: 'office-automation-erp' }, { $set: { image: './images/third-party-monitoring-bg.jpg' } })
+      await Competency.updateOne({ id: 'office-automation-erp' }, { $set: { image: './images/office-automation-erp-work.jpg', focalPoint: 'center 45%' } })
       await Competency.updateOne({ id: 'inclusive-programming' }, { $set: { image: './images/inclusive-programming-bg.jpg', focalPoint: 'center 38%' } })
     }
 
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -49,8 +49,8 @@ export function applyCompetencyImages(list) {
     if (c.id === 'third-party-monitoring' && (!c.image || c.image === './images/third-party-monitoring-bg.jpg')) {
       return { ...c, image: './images/office-automation-erp-bg.jpg' }
     }
-    if (c.id === 'office-automation-erp' && (!c.image || c.image === './images/office-automation-erp-bg.jpg')) {
-      return { ...c, image: './images/third-party-monitoring-bg.jpg' }
+    if (c.id === 'office-automation-erp') {
+      return { ...c, image: './images/office-automation-erp-work.jpg', focalPoint: 'center 45%' }
     }
     if (c.id === 'inclusive-programming' && (!c.image || c.image === './images/inclusive-programming-bg.png')) {
       return { ...c, image: './images/inclusive-programming-bg.jpg', focalPoint: 'center 38%' }
